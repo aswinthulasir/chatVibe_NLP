82d267ae53c589ef097e2133baa3a1ac412c75c1
Nandhana SB <nandhanaesbee16@gmail.com>
2024-03-04 01:33:14 +0530

    Added chat_upload model

--- chat_upload/models.py
+++ chat_upload/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class chat_upload(models.Model):
+    chat_id = models.AutoField(primary_key=True)
+    chat_file = models.FileField(upload_to='chat_files/')
+    chat_startdatetime = models.DateTimeField()
+    chat_startampm = models.CharField(max_length=2,)
+    chat_enddatetime = models.DateTimeField()
+    chat_endampm = models.CharField(max_length=2,)
+
+    def __str__(self):
+        return self.chat_file
