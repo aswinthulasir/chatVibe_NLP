1e025ff024101401d9fb5042f5a7dc4882fd346a
Nandhana SB <nandhanaesbee16@gmail.com>
2024-04-05 15:45:39 +0530

    Added tokenizer with vectorization

--- utils/Tokenizer.py
+++ utils/Tokenizer.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import tensorflow as tf
 from keras.layers import TextVectorization
-from keras.preprocessing.sequence import pad_sequences
+#from keras.preprocessing.sequence import pad_sequences
 
 def CleanText_csv(csv_file):
     # """
@@ -10,10 +10,10 @@ def CleanText_csv(csv_file):
     # csv_file: path to the csv file
     # """
     df = pd.read_csv(csv_file)
-    for index, value in df['text']:
-        df['text'][index] = Tokenize(value)
+    df['text_tokenized'] = df['text'].apply(Tokenize)
+    return df
 
-    df.to_csv('csv_file', index=False)
+   # df.to_csv('csv_file', index=False)
 
 
 
@@ -28,9 +28,15 @@ def Tokenize(texts):
     vectorizer = TextVectorization(max_tokens=100, output_mode='int')
     vectorizer.adapt(texts)  # Fit the vectorizer on the texts
     sequences = vectorizer(texts)  # Convert texts to sequences of indices
-    print(sequences)
+    #print(sequences)
     return sequences
 
+#texts = ["hello i am nandhana","are you ok","Im not well"]
+#Tokenize(texts)
+#csvfile = r'C:\Users\NANDHANA ESBEE\Desktop\CHATViBE\chatvibe\data\dataset\new.csv'
+csvfile = r"C:\Users\NANDHANA ESBEE\Desktop\CHATViBE\chatvibe\data\dataset\text.csv"
+result =  CleanText_csv(csvfile)
+print(result)
 
 
 
