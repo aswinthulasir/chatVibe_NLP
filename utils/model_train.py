31c941992da569245e35ed5a6580850bb8cbe5d9
Nandhana SB <nandhanaesbee16@gmail.com>
2024-04-29 12:40:17 +0530

    model training worked successfully

--- utils/model_train.py
+++ utils/model_train.py
@@ -25,15 +25,11 @@ X_test_features = vectorizer.transform(X_test)
 
 # Model training (using Logistic Regression in this example)
 # Create the GradientBoostingClassifier model
-model = GradientBoostingClassifier(n_estimators=100, learning_rate=0.05, max_depth=3)
+model = GradientBoostingClassifier(n_estimators=100, learning_rate=0.01, max_depth=3)
 
 
 # Train the model on the features and labels
 model.fit(X_train_features, y_train )
-
-
-#model = LogisticRegression(solver='lbfgs')  # Choose appropriate solver
-#model.fit(X_train_features, y_train)
 
 # Model evaluation (using accuracy in this example)
 from sklearn.metrics import accuracy_score
@@ -41,15 +37,21 @@ y_pred = model.predict(X_test_features)
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
 
+
+#model = LogisticRegression(solver='lbfgs')  # Choose appropriate solver
+#model.fit(X_train_features, y_train)
+
+
+
 # Make predictions on new data (optional)
 # new_text = "I am feeling okay."
 # new_text_features = vectorizer.transform([new_text])
 # prediction = model.predict(new_text_features)
 # print("Predicted sentiment:", prediction[0])
  
-for item in df['text']:
-    new_text_features = vectorizer.transform([item])
-    prediction = model.predict(new_text_features)
-    print("Predicted sentiment:", prediction[0])
-    print(item)
-    print('------------------')
+# for item in df['text']:
+#     new_text_features = vectorizer.transform([item])
+#     prediction = model.predict(new_text_features)
+#     print("Predicted sentiment:", prediction[0])
+#     print(item)
+#     print('------------------')
