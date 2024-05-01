6a622041ddb34dad18d2c37e552bf135169a1f21
Nandhana SB <nandhanaesbee16@gmail.com>
2024-05-01 20:08:52 +0530

    emotionlabel function worked successfully

--- utils/resultmodel.py
+++ utils/resultmodel.py
@@ -1,52 +1,17 @@
-# import pickle
-# def emotionlabel(df):
-    
-#     # Load the model from the file
-#     with open('chatvibe/pipe.pickle', 'rb') as f:
-#         loaded_model = pickle.load(f)
-#     # Use the loaded model for prediction
-#     # for item in df['Text_preprocess']:
-#     #     df['prediction'] = loaded_model.predict([item])
-    
-#     # emotions = df['prediction'].unique()
-#     # # Count emotion occurrences
-#     # emotion_counts = {}
-#     # for emotion in emotions:
-#     #   emotion_counts[emotion] = 0
-#     #   for row in df['prediction']:
-#     #     if emotion in row:
-#     #       emotion_counts[emotion] += 1
-#     text = "I am happy"
-#     prediction = loaded_model.predict([text])
-#     print(prediction)
 
-#     #return emotion_counts
 import pickle
 import pandas as pd
-df = pd.read_csv('data/dataset/new.csv')
+import collections
+# df = pd.read_csv('data/dataset/new.csv')
+# df =df.dropna()
+
 def emotionlabel(df):
     #Load the model from the file
-    emotions = []
     with open('data/trainedmodels/pipe.pickle', 'rb') as f:
         loaded_model = pickle.load(f)
-    for row in df['text']:
-        emotions.append(loaded_model.predict([row]))
-    uni = list(emotions)
-    print(uni)
-    # df['prediction'] = emotions
-    # unique = df['prediction'].
-    # # Count emotion occurrences
-    # emotion_counts = {}
-    # for emotion in unique:
-    #   emotion_counts[emotion] = 0
-    #   for row in df['prediction']:
-    #     if emotion in row:
-    #       emotion_counts[emotion] += 1
+    emotions = loaded_model.predict(df['Text_preprocess'])
+    count = collections.Counter(emotions)
+    print(count)
+    return count
 
-    # print(emotion_counts)
-    # Use the loaded model for prediction
-    # new_review = "feeling sick."
-    # prediction = loaded_model.predict([new_review])
-    #return prediction
-
-emotionlabel(df)
+#emotionlabel(df)
