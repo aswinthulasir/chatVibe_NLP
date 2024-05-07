0178962713cdcf0b91abd16ee46e7798ffe8820a
Nandhana SB <nandhanaesbee16@gmail.com>
2024-05-07 13:47:02 +0530

    changed model data

--- chat_upload/models.py
+++ chat_upload/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class chat_upload(models.Model):
     chat_id = models.AutoField(primary_key=True)
     chat_file = models.FileField(upload_to='data/ipdata/', blank=False)
-    Name = models.CharField(max_length=100, null=True, blank=True)
+    Name = models.CharField(max_length=100, null=False, blank=False)
     chat_startdate = models.DateField(null=True, blank=True)
     chat_enddate = models.DateField(null=True, blank=True)
     def __str__(self):
